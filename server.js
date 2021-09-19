d653e145e1315be033dd234865ce3967d98506ab
shivamvora <shivampatel27102001@gmail.com>
2021-09-19 11:24:35 +0530

    created out complete folder structure

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+require( "dotenv" ).config( { path: "./config.env" } )
+
 const express = require( 'express' )
+
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 
 app.listen( PORT, () => console.log( `Server running on port ${PORT}` ) );
